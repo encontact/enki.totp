a463a51cabd88eed4799c43610223b0709e7c747
Reinaldo Coelho Sartorelli <reinaldo.coelho@gmail.com>
2017-01-02 18:03:36 -0200

    Change to make console open and change number.

--- src/totpcli/Program.cs
+++ src/totpcli/Program.cs
@@ -1,22 +1,66 @@
 ﻿using System;
 using enki.totp;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace ConsoleApplication
 {
     public class Program
     {
-        public static void Main(string[] args)
+        static public IConfigurationRoot Configuration { get; set; }
+
+        public static void Main()
         {
-            var Key = "GEZDGNBVGY3TQOJQGEZDGNBVGY3TQOJQ";
-            if(args.Length > 2 && args[0] == "-k") {
-                Key = args[1];
+            var builder = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("config.json");
+            Configuration = builder.Build();
+
+            var timeInSeconds = Convert.ToInt32(Configuration["TimeInSeconds"]);
+            var digits = Convert.ToInt32(Configuration["Digits"]);
+            var totp = new Totp(Configuration["Key"], timeInSeconds, digits);
+
+            //var cancellationToken = new CancellationToken();
+
+            // TODO: Loop infinito, resolver pro cara não ter q matar o programa.
+            var subtractTime = timeInSeconds;
+            var exit = false;
+            while (!exit) {
+                Console.Clear();
+                Console.WriteLine(string.Concat("Seu codigo atual: ", totp.getCodeString()));
+                
+                // Console.WriteLine(string.Format("\r\n Faltam {0} segundos para trocar o número.", subtractTime));
+                // subtractTime--;
+                // if(subtractTime < 0) subtractTime = timeInSeconds;
+                
+                // if(Console.ReadKey() == ConsoleKey.Escape) exit = true;
+                Console.Write("\r\nFeche a janela para sair... ");
+
+                System.Threading.Tasks.Task.Delay(1000).Wait();
             }
-
-            var totp = new Totp(Key, 30, 8);
-            Console.WriteLine(string.Concat("Seu codigo atual: ", totp.getCodeString()));
-
-            Console.WriteLine("Pressione qualquer tecla para encerrar.");
-            Console.ReadKey();
+            
+            // // while (Console.ReadKey().Key != ConsoleKey.Enter) {
+            // //     Console.Clear();
+            // //     Console.WriteLine(string.Concat("Seu codigo atual: ", totp.getCodeString()));
+            // //     Console.Write("Precione <Enter> para sair... ");
+            // // }
+            // //ConsoleKeyInfo cki;
+            // int cki;
+            // // Prevent example from ending if CTL+C is pressed.
+            // Console.TreatControlCAsInput = true;
+            // do 
+            // {
+            //     Console.Clear();
+            //     Console.WriteLine(string.Concat("Seu codigo atual: ", totp.getCodeString()));
+            //     Console.WriteLine("Press the q key to quit: \n");
+            //     // Console.Write(" --- You pressed ");
+            //     // if((cki.Modifiers & ConsoleModifiers.Alt) != 0) Console.Write("ALT+");
+            //     // if((cki.Modifiers & ConsoleModifiers.Shift) != 0) Console.Write("SHIFT+");
+            //     // if((cki.Modifiers & ConsoleModifiers.Control) != 0) Console.Write("CTL+");
+            //     // Console.WriteLine(cki.Key.ToString());
+            //     cki = Console.Read();
+            // //} while (cki.Key != ConsoleKey.Escape);
+            // } while (cki != 'q');
         }
     }
 }
