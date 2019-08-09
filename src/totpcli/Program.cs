19834bdc38608586584dbb34abe0fb19e6280435
reinaldo <reinaldo.coelho@gmail.com>
2019-08-09 14:37:13 -0300

    Updating version.

--- src/totpcli/Program.cs
+++ src/totpcli/Program.cs
@@ -21,20 +21,12 @@ namespace ConsoleApplication
             var digits = Convert.ToInt32(Configuration["Digits"]);
             var totp = new Totp(Configuration["Key"], timeInSeconds, digits);
 
-            // TODO: Loop infinito, resolver pro cara não ter q matar o programa.
-            //var subtractTime = timeInSeconds;
             var exit = false;
             while (!exit)
             {
                 Console.Clear();
                 Console.WriteLine(string.Concat("Seu codigo atual: ", totp.getCodeString()));
                 Console.WriteLine($"Faltam {totp.RemainingSeconds()} segundos para trocar o número.");
-
-                // Console.WriteLine(string.Format("\r\n Faltam {0} segundos para trocar o número.", subtractTime));
-                // subtractTime--;
-                // if(subtractTime < 0) subtractTime = timeInSeconds;
-
-                // if(Console.ReadKey() == ConsoleKey.Escape) exit = true;
                 Console.Write("\r\nFeche a janela para sair... ");
 
                 Task.Delay(1000).Wait();
